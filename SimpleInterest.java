0a89a3665f7a4294073e59e381f50f7526bb4791
JohnOye <66364739+JohnOye@users.noreply.github.com>
2021-02-07 13:04:26 -0800

    Add files via upload
    Simple Interest Main class

---
+++ SimpleInterest.java
@@ -0,0 +1,11 @@
+package jayo.simplified;
+
+public class SimpleInterest {
+public static double calculate(double P, double T, double R) {
+  return P * T * R / 100;
+}
+  public static void main(String[] args) {
+  
+  }
+
+}

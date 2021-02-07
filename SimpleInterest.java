8df8cd343e77f3272be151e130b84c768a85e1f1
JohnOye <66364739+JohnOye@users.noreply.github.com>
2021-02-07 13:06:42 -0800

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,5 +1,7 @@
-package jayo.simplified;
-
+/*Author(John Oye)
+Version(1.0)
+Created by John Oye to Calculate Simple Interest using PTR/100
+*/
 public class SimpleInterest {
 public static double calculate(double P, double T, double R) {
   return P * T * R / 100;
